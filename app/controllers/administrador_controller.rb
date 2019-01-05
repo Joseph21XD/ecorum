8b1d3009d6a114ce030e2506571ba2ebe9212752
Joseph <ramirezjantonio@hotmail.com>
2019-01-05 11:18:13 -0600

    agregar usuarios y verlos

--- app/controllers/administrador_controller.rb
+++ app/controllers/administrador_controller.rb
@@ -5,6 +5,8 @@ class AdministradorController < ApplicationController
 		#no tiene html ya que es una respuesta json a javascript mediante ajax a jquery
 	end
 	def usuarios
+		@tipo = TipoUsuario.find(session[:user_type])
+		@usuarios = Usuario.all.order("nombre ASC")
 	end
 	def comprobacion
 	end
--- app/controllers/organizacion_controller.rb
+++ app/controllers/organizacion_controller.rb
@@ -2,5 +2,7 @@ class OrganizacionController < ApplicationController
 	def perfil
 	end
 	def evento
+		@tipo = TipoUsuario.find(session[:user_type])
+		@usuarios = Usuario.all
 	end
 end
