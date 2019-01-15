84eaaae80eba762ad37cbdd892636a42338e0f54
Joseph <ramirezjantonio@hotmail.com>
2019-01-15 13:35:06 -0600

    imagenes evidencia

--- app/models/evidencium.rb
+++ app/models/evidencium.rb
@@ -1,3 +1,5 @@
 class Evidencium < ApplicationRecord
   belongs_to :comprobacion
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190114210932) do
+ActiveRecord::Schema.define(version: 20190115191928) do
 
   create_table "comentarios", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "usuario_id"
@@ -61,6 +61,10 @@ ActiveRecord::Schema.define(version: 20190114210932) do
     t.bigint "comprobacion_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["comprobacion_id"], name: "index_evidencia_on_comprobacion_id"
   end
 
